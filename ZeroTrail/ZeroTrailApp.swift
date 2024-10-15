5b3ef652a6faeb0af702c165193ba07b0fb04815
wolfayet0855 <online_alt02@yahoo.com>
2024-10-15 14:25:52 -0400

    Initial Commit

---
+++ ZeroTrail/ZeroTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroTrailApp.swift
+//  ZeroTrail
+//
+//  Created by user on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ZeroTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
